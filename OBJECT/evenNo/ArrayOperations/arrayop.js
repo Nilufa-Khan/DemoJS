b9da31d95f7b11a1b3f90e8732cb153bf26e520a
Nilufa Khan <khannilufa5@gmail.com>
2022-09-30 12:48:35 +0530

    ini

---
+++ OBJECT/evenNo/ArrayOperations/arrayop.js
@@ -0,0 +1,11 @@
+// insertion  .push(),unshift()
+//removal      .pop(), shift()
+let colors = ["red", "black","white"]
+colors.push("maroon","babypink");
+console.log(colors)
+colors.push("yellow");
+console.log(colors)
+colors.unshift("pink",3,"orange")
+console.log(colors)
+colors.shift()
+console.log(colors)

3e33bb27a0a5b07c1415c20082eadacc5fde50e2
Nilufa Khan <khannilufa5@gmail.com>
2022-09-30 18:07:00 +0530

    ini

--- js/function/function.js
+++ js/function/function.js
@@ -1,7 +1,8 @@
 //anyonumnus function
-
+// as per rule in anyonumnus function there should no function name
 const cube = function(number)
 {
     return number*number*number
 }
+
 console.log("Cube of 3 : "+cube(3))

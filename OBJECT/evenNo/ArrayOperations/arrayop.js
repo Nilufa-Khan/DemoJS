13799cd92a265c4c1b69c64921af367ed6b9ecf6
Nilufa Khan <khannilufa5@gmail.com>
2022-09-30 13:04:51 +0530

    ini

--- OBJECT/evenNo/ArrayOperations/arrayop.js
+++ OBJECT/evenNo/ArrayOperations/arrayop.js
@@ -4,11 +4,20 @@ let colors = ["red", "black","white"]
 colors.push("maroon","babypink");
 console.log(colors)
 //push element at last index
+console.log("====push====")
 colors.push("yellow");
 console.log(colors)
 // unshift add elemet at 0th index
+console.log("====unshift====")
 colors.unshift("pink","orange")
 console.log(colors)
-colors.shift()
+//pop remove the last index element
+console.log("====pop====")
+console.log(colors.pop())
 console.log(colors)
+//shift remove 0th index element
+console.log("====shift====")
+console.log(colors.shift())
+console.log(colors)
+
 

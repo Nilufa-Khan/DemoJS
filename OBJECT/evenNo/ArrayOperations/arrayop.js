36d9795375d0eec1fafecd0dd0d9b1783f7bc7de
Nilufa Khan <khannilufa5@gmail.com>
2022-09-30 13:08:38 +0530

    ini

--- OBJECT/evenNo/ArrayOperations/arrayop.js
+++ OBJECT/evenNo/ArrayOperations/arrayop.js
@@ -6,11 +6,13 @@ console.log("Initially elements in array are :\n" + colors+" ")
 console.log("====push====")
 console.log(colors.push("maroon","babypink"));
 console.log(colors)
+
 //push element at last index
 
 console.log("====push====")
 colors.push("yellow");
 console.log(colors)
+
 // unshift add elemet at 0th index
 
 console.log("====unshift====")
